d5528ca66b03cf756a80176c9f94786bb92cde82
LuaGeo <ldeoliveiratech@gmail.com>
2025-06-13 13:12:21 +0200

    fix deployment config2

--- app/models/food_detector.py
+++ app/models/food_detector.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class FoodDetector:
     def __init__(self):
-        self.model = YOLO('yolov8n.pt')
+        self.model = YOLO('yolov8n')
     
     def detect_food(self, image):
         results = self.model(image)
